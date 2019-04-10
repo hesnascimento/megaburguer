c75d7cf50e5edc13e3655262cce12c07560cad72
Henrique Souza Nascimento <hesnascimento@gmail.com>
2019-04-09 22:37:25 -0300

    Configuração do ESLint Configuração do EditorConfig Testes para IngredientesService.js

--- src/services/IngredientesService.js
+++ src/services/IngredientesService.js
@@ -1,19 +1,30 @@
 class IngredientesService {
-    constructor(ingredientes) {
-        if (!Array.isArray(ingredientes)) return null;
-        this.ingredientes = ingredientes;
-    }
+  /**
+   * Inicializa o serviço com os ingrediente passados por parâmetro
+   *
+   * @param {Array} ingredientes
+   */
+  constructor(ingredientes) {
+    if (!Array.isArray(ingredientes)) throw Error('Os ingredientes devem ser um array de ingredientes');
+    this.ingredientes = ingredientes;
 
-    getIngredienteById(id) {
-        if (typeof id !== 'sting') return undefined;
+    this.getIngredienteById = this.getIngredienteById.bind(this);
+    this.getAll = this.getAll.bind(this);
+  }
 
-        // this.ingredientes.
-        return { nome: 'Ovo' };
-    }
+  /**
+   *Retorna o ingrediente dado o ID
+   * @param {string} id
+   * @returns {Object}
+   */
+  getIngredienteById(id) {
+    if (typeof id !== 'string') return undefined;
+    return this.ingredientes.find(ingrediente => ingrediente.id === id);
+  }
 
-    getAll() {
-        return [];
-    }
-};
+  getAll() {
+    return this.ingredientes;
+  }
+}
 
 module.exports = IngredientesService;
--- test/services/IngredientesService.spec.js
+++ test/services/IngredientesService.spec.js
@@ -1,109 +1,115 @@
 const { expect } = require('chai');
 const IngredientesServices = require('../../src/services/IngredientesService');
 
-let ingredientes = [];
-let lanches = [];
+const ingredientes = [
+  {
+    id: 'alface',
+    nome: 'Alface',
+    preco: 0.4,
+  },
+  {
+    id: 'bacon',
+    nome: 'Bacon',
+    preco: 2,
+  },
+  {
+    id: 'hamburguer',
+    nome: 'Hambúrguer de carne',
+    preco: 3,
+  },
+  {
+    id: 'ovo',
+    nome: 'Ovo',
+    preco: 0.8,
+  },
+  {
+    id: 'queijo',
+    nome: 'Queijo',
+    preco: 1.5,
+  },
+];
 
-before = () => {
-    ingredientes = [
-        {
-            id: 'alface',
-            nome: 'Alface',
-            preco: 0.4,
-        },
-        {
-            id: 'bacon',
-            nome: 'Bacon',
-            preco: 2,
-        },
-        {
-            id: 'hamburguer',
-            nome: 'Hambúrguer de carne',
-            preco: 3,
-        },
-        {
-            id: 'ovo',
-            nome: 'Ovo',
-            preco: 0.8,
-        },
-        {
-            id: 'queijo',
-            nome: 'Queijo',
-            preco: 1.5,
-        },
-    ];
+// let lanches = [];
 
-    lanches = [
-        {
-            id: 'xbacon',
-            lanche: 'X-Bacon',
-            ingredientes: [
-                'bacon',
-                'hamburguer',
-                'queijo',
-            ],
-        },
-        {
-            id: 'xburguer',
-            lanche: 'X-Burguer',
-            ingredientes: [
-                'hamburguer',
-                'queijo',
-            ],
-        },
-        {
-            id: 'xegg',
-            lanche: 'X-Egg',
-            ingredientes: [
-                'ovo',
-                'hamburguer',
-                'queijo',
-            ],
-        },
-        {
-            id: 'xeggbacon',
-            lanche: 'X-Egg Bacon',
-            ingredientes: [
-                'ovo',
-                'bacon',
-                'hamburguer',
-                'queijo',
-            ],
-        },
-    ]
-}
+// before(() => {
+//   lanches = [
+//     {
+//       id: 'xbacon',
+//       lanche: 'X-Bacon',
+//       ingredientes: [
+//         'bacon',
+//         'hamburguer',
+//         'queijo',
+//       ],
+//     },
+//     {
+//       id: 'xburguer',
+//       lanche: 'X-Burguer',
+//       ingredientes: [
+//         'hamburguer',
+//         'queijo',
+//       ],
+//     },
+//     {
+//       id: 'xegg',
+//       lanche: 'X-Egg',
+//       ingredientes: [
+//         'ovo',
+//         'hamburguer',
+//         'queijo',
+//       ],
+//     },
+//     {
+//       id: 'xeggbacon',
+//       lanche: 'X-Egg Bacon',
+//       ingredientes: [
+//         'ovo',
+//         'bacon',
+//         'hamburguer',
+//         'queijo',
+//       ],
+//     },
+//   ];
+// });
 
 describe('IngredientesServices Class', () => {
-    const ingredientesService = new IngredientesServices(ingredientes);
-    const badIngredientesService = new IngredientesServices('teste');
+  const ingredientesService = new IngredientesServices(ingredientes);
 
-    describe('constructor', () => {
-        it('should be instance of IngredientesService', () => {
-            expect(ingredientesService).to.be.instanceOf(IngredientesServices);
-        });
+  it('should thow error if constructor param is not an array', () => {
+    expect(() => new IngredientesServices('teste')).to.throw();
+  });
 
-        it('should be null', () => {
-            expect(badIngredientesService).to.be.null();
-        })
+  describe('constructor', () => {
+    it('should be instance of IngredientesService', () => {
+      expect(ingredientesService).to.be.instanceOf(IngredientesServices);
     });
 
-    describe('getIngredienteById', () => {
-        it('should return ovo element', () => {
-            const ingrediente = ingredientesService.getIngredienteById('ovo');
-            expect(ingrediente.nome).to.be.equal('Ovo');
-        });
+    it('should have ingredientes', () => {
+      expect(ingredientesService.ingredientes).to.have.lengthOf(5);
+    });
+  });
 
-        it('should return undefined', () => {
-            const ingrediente = ingredientesService.getIngredienteById('macarrao');
-            expect(ingrediente.nome).to.be.undefined();
-        });
+  describe('getIngredienteById', () => {
+    it('should return ovo element', () => {
+      const ingrediente = ingredientesService.getIngredienteById('ovo');
+      expect(ingrediente.nome).to.be.equal('Ovo');
     });
 
-    describe('getAll', () => {
-        it('should return an array', () => {
-            const todosIngredientes = ingredientesService.getAll();
-            expect(todosIngredientes).to.be.an('array');
-        });
+    it('should return undefined because id doesnt exists', () => {
+      const ingrediente = ingredientesService.getIngredienteById('macarrao');
+      expect(ingrediente).to.be.an('undefined');
     });
 
+    it('should return undefined because parameter is not a string', () => {
+      const ingrediente = ingredientesService.getIngredienteById({ id: 'ovo' });
+      expect(ingrediente).to.be.an('undefined');
+    });
+  });
+
+  describe('getAll', () => {
+    it('should return an array', () => {
+      const todosIngredientes = ingredientesService.getAll();
+      expect(todosIngredientes).to.be.an('array');
+    });
+  });
 });
