e8c9dd76e097c54a216cc78c648e1787c2b8aaef
Henrique Souza Nascimento <hesnascimento@gmail.com>
2019-04-15 20:54:57 -0300

    Resolve CORS

--- src/express.js
+++ src/express.js
@@ -1,11 +1,27 @@
 const express = require('express');
 const monrgan = require('morgan');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const IngredientesRoutes = require('./routes/IngredientesRoutes');
 const LanchesRoutes = require('./routes/LanchesRoutes');
 
 const app = express();
+
+// Configura CORS
+
+const permitidos = process.env.CORS_LIST.split(',');
+const opcoesCors = {
+  origin: (origem, callback) => {
+    if (permitidos.indexOf(origem) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Bloqueado no CORS'));
+    }
+  },
+};
+
+app.use(cors(opcoesCors));
 
 // Aceita application/json
 app.use(bodyParser.json());
